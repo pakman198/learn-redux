ab19e32a538f33136aa5fd5655084cb17d62463a
José Francisco Herrera Hernández <pako@MacBook-Pro-de-Jose.local>
2017-10-11 19:07:16 -0500

    initial commit

---
+++ client/reducers/posts.js
@@ -0,0 +1,18 @@
+function posts(state = [], action){
+	console.log(state, action);
+	switch(action.type){
+		case 'INCREMENT_LIKES' :
+			console.log('Incrementing likes !');
+			const i = action.index;
+			return [
+				...state.slice(0, i),
+				{...state[i], 'likes': state[i].likes + 1},
+				...state.slice(i + 1)
+			];
+
+		default :
+			return state;
+	}
+}
+
+export default posts;
